6016a440b6375be94499a5d4f07f7e972d84591c
TouchIsNotAGoodCoder <64277067+Touchcreator@users.noreply.github.com>
2022-02-21 22:24:21 +0000

    Add input and ground

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import pygame
 screenSize = (700,500)
 white = (255,255,255)
 cyan = (117, 235, 225)
+green = (5, 255, 72)
+WIDTH = 700
+HEIGHT = 500
 logo = pygame.image.load("./assets/images/tard/tard_00.png")
 
 #init
@@ -19,28 +22,40 @@ pygame.display.set_icon(logo)
 
 #player
 playerImage = pygame.image.load("./assets/images/tard/tard_00.png")
+playerX = 300
+playerY = 100
 
 #platforms
-platform = pygame.Rect(100,300,400,50)
+platforms = [
+  pygame.Rect(100,300,400,50),
+  pygame.Rect(100,250,50,50),
+  pygame.Rect(450,250,50,50)
+]
 
 #game loop
 running = True
 while running:
 
-  #get input
+  #quit check
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
-  #update
+  #player input
+      keys = pygame.key.get_pressed()
+      if keys[pygame.K_a]:
+        playerX -= 2
+      if keys[pygame.K_d]:
+        playerX += 2
 
   #draw
   #bg
   screen.fill(cyan)
   #plats
-  pygame.draw.rect(screen,(),platform)
+  for p in platforms:
+    pygame.draw.rect(screen,(green),p)
   #player
-  screen.blit(playerImage,(300,0))
+  screen.blit(playerImage,(playerX,playerY))
   #screen
   pygame.display.flip()
 
